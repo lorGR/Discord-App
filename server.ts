0c24ee81f3410788c91539022c5c11d006e4ebb8
Lior Grishin <94124882+lorGR@users.noreply.github.com>
2022-07-21 15:31:56 +0300

    Starting Files

---
+++ server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app : express.Application = express();
+const port : string = process.env.PORT;
+const mongodb_uri : string = process.env.MONGODB_URI;
+
+if(mongodb_uri) {
+    mongoose
+        .connect(mongodb_uri)
+        .then(() => console.log("Connected to DB."))
+        .catch(() => console.log("Couldn't connect to DB."));
+} else {
+    console.log("Couldn't find mongodb_uri");
+}
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log(`Server is running http://localhost:${port}`);
+})
