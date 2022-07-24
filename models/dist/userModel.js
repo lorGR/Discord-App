e1a58e37003d918f2db0ffd668c0aaf9d0ac8c52
Lior Grishin <94124882+lorGR@users.noreply.github.com>
2022-07-24 14:57:51 +0300

    UserModel Exported and started validation of user Inputs

--- models/dist/userModel.js
+++ models/dist/userModel.js
@@ -1,2 +1,36 @@
 "use strict";
 exports.__esModule = true;
+var mongoose_1 = require("mongoose");
+var joi_1 = require("joi");
+var joi_password_1 = require("joi-password");
+var joiPassword = joi_1["default"].extend(joi_password_1.joiPasswordExtendCore);
+var UserSchema = new mongoose_1["default"].Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    repeatPassword: {
+        type: String,
+        required: true
+    },
+    userTag: {
+        type: Number,
+        required: true
+    }
+});
+var UserModel = mongoose_1["default"].model("users", UserSchema);
+exports["default"] = UserModel;
+var UserValidation = joi_1["default"].object({
+    email: joi_1["default"].string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    username: joi_1["default"].string().alphanum().min(3).max(16).required(),
+    password: joiPassword.string().minOfSpecialCharacters(1).minOfLowerCase(1).minOfUpperCase(1).minOfNumric(1).noWhiteSpaces().min(6).max(16).required()
+});
--- models/userModel.ts
+++ models/userModel.ts
@@ -1,1 +1,39 @@
 import mongoose from 'mongoose';
+import Joi from 'joi';
+import { joiPasswordExtendCore } from 'joi-password';
+
+const joiPassword = Joi.extend(joiPasswordExtendCore);
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    repeatPassword: {
+        type: String,
+        required: true
+    },
+    userTag: {
+        type: Number,
+        required: true
+    }
+});
+
+const UserModel = mongoose.model("users", UserSchema);
+
+export default UserModel;
+
+const UserValidation = Joi.object({
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    username: Joi.string().alphanum().min(3).max(16).required(),
+    password: joiPassword.string().minOfSpecialCharacters(1).minOfLowerCase(1).minOfUpperCase(1).minOfNumric(1).noWhiteSpaces().min(6).max(16).required()
+})
